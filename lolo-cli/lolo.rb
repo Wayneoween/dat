2b8b992494d94f901f7293a676c438ae18a85b84
Stefan Thiemann <stefan.thiemann@gmail.com>
2015-11-13 20:21:34 +0100

    add new commands
    now we have:
    
    ./lolo.rb 2 on
    ./lolo.rb 2 red
    ./lolo.rb 2 green
    ./lolo.rb my_lamp warm
    ./lolo.rb 2 warm
    ./lolo.rb 2 cold
    
    We have to set the color in HUE HSV Color, not in RGB.

--- lolo-cli/lolo.rb
+++ lolo-cli/lolo.rb
@@ -4,12 +4,24 @@ require 'rest-client'
 require 'clamp'
 require 'yaml'
 require 'json'
-require Dir.pwd + "/models/light"
+require_relative "models/light"
 
 $uri = "http://localhost:7777/api"
 $config_file = "config.yml"
 $key = nil
 $light = nil
+
+HUE_MAP = {
+  :green => (65535/(360/106.to_f)).to_i,
+  :red => (65535/(360/360.to_f)).to_i,
+  :blue => (65535/(360/233.to_f)).to_i,
+  :white => 1
+}
+
+TEMP_MAP = {
+  :warm => 500,
+  :cold => 153
+}
 
 def set_api_key
   if $key = get_cfg("api_key")
@@ -81,18 +93,18 @@ class SubcommandLightGroup
     #    return true
     #  end
     #end
-    Light.all.each do |light|
-      if light.name == other_object
-        $light = light
-        return true
-      end
+    if $light = Light.find_by_name(other_object)
+      return true
+    end
+    if $light = Light.find_by_id(other_object)
+      return true
     end
     return false
   end
 
   # Used to beautify the Clamp usage.
   def to_s
-    return "<light|group>"
+    return "<light|group> <on|off>"
   end
 end
 
@@ -104,7 +116,7 @@ Clamp do
     end
   end
 
-  subcommand SubcommandLightGroup.new, "Switch a light or group on/off." do
+  subcommand SubcommandLightGroup.new, "Switch a light (by name or id) or group on/off." do
 
     subcommand "on", "Switch on." do
       def execute
@@ -118,6 +130,42 @@ Clamp do
       end
     end
 
+    subcommand "red", "Set light color to red." do
+      def execute
+        $light.set_color(HUE_MAP[:red])
+      end
+    end
+
+    subcommand "blue", "Set light color to blue." do
+      def execute
+        $light.set_color(HUE_MAP[:blue])
+      end
+    end
+
+    subcommand "green", "Set light color to green." do
+      def execute
+        $light.set_color(HUE_MAP[:green])
+      end
+    end
+
+    subcommand "white", "Set light color to white." do
+      def execute
+        $light.set_color(HUE_MAP[:white])
+      end
+    end
+
+
+    subcommand "warm", "Set light temperature to warm." do
+      def execute
+        $light.set_temp(TEMP_MAP[:warm])
+      end
+    end
+
+    subcommand "cold", "Set light temperature to cold." do
+      def execute
+        $light.set_temp(TEMP_MAP[:cold])
+      end
+    end
   end
 
 end
--- lolo-cli/models/light.rb
+++ lolo-cli/models/light.rb
@@ -16,8 +16,12 @@ class Light
     return lights
   end
 
-  def self.find(id)
-    response = RestClient.get $uri + "/#{$key}/lights/#{id}"
+  def self.find_by_id(id)
+    begin
+      response = RestClient.get $uri + "/#{$key}/lights/#{id}"
+    rescue
+      return nil
+    end
     response = JSON.parse(response)
     l = Light.new
     l.id = id
@@ -25,6 +29,29 @@ class Light
     l.name = response["name"]
     l.on = response["state"]["on"]
     return l
+  end
+
+  def self.find_by_name(name)
+    response = RestClient.get $uri + "/#{$key}/lights"
+    response = JSON.parse(response)
+    response.each do |nr, light|
+      next unless light["name"] == name
+      l = Light.new
+      l.id = nr
+      l.manufacturer = light["manufacturer"]
+      l.name = light["name"]
+      l.on = light["state"]["on"]
+      return l
+    end
+    return nil
+  end
+
+  def set_color(hue)
+    RestClient.put $uri + "/#{$key}/lights/#{id}/state", {:hue => hue}.to_json
+  end
+
+  def set_temp(temp)
+    RestClient.put $uri + "/#{$key}/lights/#{id}/state", {:ct => temp}.to_json
   end
 
   def turn_on
