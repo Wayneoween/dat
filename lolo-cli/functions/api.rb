41db42681bbe0c75f1b1a87777b621c310a142f5
Marius Schuller <marius.schuller@googlemail.com>
2015-11-18 09:54:18 +0100

    example for restructuring; ToDo: clamp
    Signed-off-by: Marius Schuller <marius.schuller@googlemail.com>

---
+++ lolo-cli/functions/api.rb
@@ -0,0 +1,53 @@
+def set_api_key
+  if $key = get_cfg("api_key")
+    return $key
+  end
+
+  begin
+    response = RestClient.post $uri,
+      { 'devicetype' => "myappbar" }.to_json,
+      :content_type => :json,
+      :accept => :json
+  rescue
+    puts "Unable to retrieve API-Key.\nPlease unlock your Gateway first!"
+    exit 1
+  end
+  response = JSON.parse(response)
+  if response && response.first["success"]
+    key = response.first["success"]["username"]
+    set_cfg("api_key", key)
+    $key = key
+    return key
+  end
+end
+
+def set_cfg(key, value)
+  File.open($config_file, "w") do |file|
+    file.write({key => value}.to_yaml)
+  end
+end
+
+def get_cfg(key)
+  unless File.exist?($config_file)
+    File.open($config_file, 'w'){}
+  end
+  cfg = YAML.load(File.read($config_file))
+  if cfg && cfg[key]
+    return cfg[key]
+  end
+end
+
+def get_lights
+  puts "Getting all lights..."
+  ret = {}
+  Light.all.each do |light|
+    ret[light.id] =  {:name =>light.name}
+  end
+  puts ret.to_json
+end
+
+def get_light_by_id(id)
+  light = Light.find(id)
+  ret = {:id => light.id, :on => light.on, :name => light.name}.to_json
+  puts ret
+end
