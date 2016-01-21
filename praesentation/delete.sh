10621c5958a52e6b07efda5b8bd571720bb66ade
Patrick Wildt <patrick@blueri.se>
2016-01-21 17:51:21 +0100

    doit: delete more

--- praesentation/delete.sh
+++ praesentation/delete.sh
@@ -2,10 +2,12 @@
 set -ex
 
 # delete all (as known in this script), turn off lights
-cd ../lolo-cli
+pushd ../lolo-cli/
 ./lolo.rb light L1 off
 ./lolo.rb light L2 off
 ./lolo.rb light L3 off
 ./lolo.rb delete group All
 ./lolo.rb delete group G1
 ./lolo.rb delete group G2
+rm -f *_cache.yml
+popd
